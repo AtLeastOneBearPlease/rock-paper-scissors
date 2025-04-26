85ead5e78bb08b08bf267308cc119ea8f2598ad1
Alex Walters <197534792+AtLeastOneBearPlease@users.noreply.github.com>
2025-04-26 08:36:24 -0500

    feat: Add playRound and logic
    This allows you to play one round of rock, paper,
    scissors with the computer

--- javascript.js
+++ javascript.js
@@ -2,13 +2,13 @@ function getComputerChoice(){
     let choice = Math.floor( (Math.random() * 3) );
 
     if(choice === 0) {
-        return "Rock";
+        return "ROCK";
     }
     else if(choice === 1) {
-        return "Paper";
+        return "PAPER";
     }
     else if(choice === 2) {
-        return "Scissors";
+        return "SCISSORS";
     }
     else {
         return "ERROR";
@@ -21,8 +21,51 @@ function getHumanChoice(){
 
 function playRound(humanChoice, computerChoice){
     humanChoice = humanChoice.toUpperCase();
+
+    let humanWin = "The human wins!";
+    let computerWin = "The computer wins!";
+    
+    if(humanChoice === computerChoice) {
+        console.log("This round was a tie!");
+    }
+    else if(humanChoice === "ROCK") {
+        switch(computerChoice) {
+            case "SCISSORS":
+                console.log(humanWin);
+                break;
+            case "PAPER":
+                console.log(computerWin);
+                break;
+        }
+    }
+    else if(humanChoice === "PAPER") {
+        switch(computerChoice) {
+            case "ROCK":
+                console.log(humanWin);
+                break;
+            case "SCISSORS":
+                console.log(computerWin);
+                break;
+        }
+    }
+    else if(humanChoice === "SCISSORS") {
+        switch(computerChoice) {
+            case "PAPER":
+                console.log(humanWin);
+                break;
+            case "ROCK":
+                console.log(computerWin);
+            
+        }
+    }
+
 }
 
 let humanScore = 0;
 let computerScore = 0;
 
+/*
+ROCK + PAPER
+PAPER + SCISSORS
+SCISSORS + ROCK
+*/
