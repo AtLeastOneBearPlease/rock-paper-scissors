9e329d5d6eac2388f84aaf491f7aafbf35516031
Alex Walters <197534792+AtLeastOneBearPlease@users.noreply.github.com>
2025-04-26 08:12:07 -0500

    feat: Add getComputerChoice function

--- javascript.js
+++ javascript.js
@@ -1,1 +1,20 @@
-console.log("Butt");
+function getComputerChoice(){
+    let choice = (Math.random() * 3).floor();
+
+    if(choice === 0) {
+        return "Rock";
+    }
+    else if(choice === 1) {
+        return "Paper";
+    }
+    else if(choice === 2) {
+        return "Scissors";
+    }
+    else {
+        return "ERROR";
+    }
+}
+
+for(let i=10; i > 0; i--){
+    console.log(getComputerChoice());
+}
